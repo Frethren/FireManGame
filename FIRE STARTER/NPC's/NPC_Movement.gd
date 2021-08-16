935b917946defaeeafd777a4d9a228899c61777a
Miles Ghiggioli <milghi28@kingshigh.school.nz>
2021-08-16 12:31:11 +1200

    FRICK
    AHHHHHHHHHHHH

--- FIRE STARTER/NPC's/NPC_Movement.gd
+++ FIRE STARTER/NPC's/NPC_Movement.gd
@@ -5,25 +5,29 @@ var path_node = 0
 
 var speed = 10
 
-onready var nav = $"../Nav"
+onready var nav = $"../Navigation"
 onready var player = $"../Player"
 
+var target = Vector3.ZERO
 
 func _ready():
-	pass
+	target = Vector3(rand_range(-10,10),rand_range(-10,10),rand_range(-10,10))
+	target = nav.get_closest_point(target)
+	path = nav.get_simple_path(global_transform.origin, target)
+	
 
 func _physics_process(delta):
 	if path_node < path.size():
 		var direction = (path[path_node] - global_transform.origin)
 		if direction.length() < 1:
-			path.node += 1
+			path_node += 1
 		else:
 			move_and_slide(direction.normalized() * speed, Vector3.UP)
 
 
-func move_to(target_pos):
-	path = nav.get_simple_path(global_transform.origin, target_pos)
-	path_node = 0
+#func move_to(target_pos):
+#	path = nav.get_simple_path(global_transform.origin, target_pos)
+#	path_node = 0
 
 func _on_Timer_timeout():
-	move_to()
+	move_and_slide(target)
